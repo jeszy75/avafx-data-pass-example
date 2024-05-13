1894109f0d0f34e32f31cdacaf8f484a2cc518dd
Péter Jeszenszky <jeszenszky.peter@inf.unideb.hu>
2024-05-13 20:17:16 +0200

    initial import

---
+++ src/main/java/datapass/ex3/FirstController.java
@@ -0,0 +1,37 @@
+package datapass.ex3;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import org.tinylog.Logger;
+
+import java.io.IOException;
+
+public class FirstController {
+
+    @FXML
+    private TextField nameField;
+
+    @FXML
+    private void initialize() {
+        nameField.setText(System.getProperty("user.name"));
+    }
+
+    @FXML
+    private void handleNextButton(ActionEvent event) throws IOException {
+        Logger.info("Name entered: {}", nameField.getText());
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("second.fxml"));
+        Parent root = fxmlLoader.load();
+        SecondController controller = fxmlLoader.getController();
+        controller.setName(nameField.getText());
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
+
+}
